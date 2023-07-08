e3eacfd119aaf07ca6fd6cda1666d7637b4a471f
Oleg Yatmanov <o_yatmanov@mail.ru>
2023-07-08 18:06:12 +0500

    Семинар 14 домашнее задание

---
+++ task54/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Write(input);
+  int output = int.Parse(ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Write(array[i, j] + " ");
+    }
+    WriteLine();
+  }
+}
